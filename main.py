a2c41fbd83e54f67de1bb87883892d53c9d5b072
Antonina Novikova <novikovantoninalex@gmail.com>
2025-02-04 17:14:07 +0400

    add file

---
+++ main.py
@@ -0,0 +1,20 @@
+import transliterate
+import num2words
+
+from transliterate import translit, get_available_language_codes
+from num2words import num2words
+
+def main():
+    numbers=('78','15','3','40','8')
+    
+    text = ("""Ladies and gentlemen, I'm 78 years old and I finally got 15 minutes of fame once in a lifetime and I guess that this is mine. People have also told me to make these next few minutes escruciatingly embarrassing and to take vengeance of my enemies. Neither will happen.
+More than 3 years ago I moved to Novo-Novsk, but worked on new Magnetic Storage for last 40. When I was 8...
+            """)
+
+    print(translit(text, 'ru'))
+
+    for number in numbers:
+        print(f'{number} -', translit(num2words(number),'ru'))
+
+if __name__ == '__main__':
+    main()
